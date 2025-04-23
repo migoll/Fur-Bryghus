9a604157d04a2618cdd296b2961f96d84472cc98
Gabbana <chris.lund02@gmail.com>
2025-04-23 10:20:59 +0200

    button

--- my-nuxt-app/.nuxt/components.d.ts
+++ my-nuxt-app/.nuxt/components.d.ts
@@ -12,7 +12,8 @@ type HydrationStrategies = {
 }
 type LazyComponent<T> = (T & DefineComponent<HydrationStrategies, {}, {}, {}, {}, {}, {}, { hydrated: () => void }>)
 interface _GlobalComponents {
-      'TheHeader': typeof import("../src/components/TheHeader.vue")['default']
+      'Button': typeof import("../src/components/Button.vue")['default']
+    'TheHeader': typeof import("../src/components/TheHeader.vue")['default']
     'NuxtWelcome': typeof import("../node_modules/nuxt/dist/app/components/welcome.vue")['default']
     'NuxtLayout': typeof import("../node_modules/nuxt/dist/app/components/nuxt-layout")['default']
     'NuxtErrorBoundary': typeof import("../node_modules/nuxt/dist/app/components/nuxt-error-boundary")['default']
@@ -36,7 +37,8 @@ interface _GlobalComponents {
     'Body': typeof import("../node_modules/nuxt/dist/head/runtime/components")['Body']
     'NuxtIsland': typeof import("../node_modules/nuxt/dist/app/components/nuxt-island")['default']
     'NuxtRouteAnnouncer': IslandComponent<typeof import("../node_modules/nuxt/dist/app/components/server-placeholder")['default']>
-      'LazyTheHeader': LazyComponent<typeof import("../src/components/TheHeader.vue")['default']>
+      'LazyButton': LazyComponent<typeof import("../src/components/Button.vue")['default']>
+    'LazyTheHeader': LazyComponent<typeof import("../src/components/TheHeader.vue")['default']>
     'LazyNuxtWelcome': LazyComponent<typeof import("../node_modules/nuxt/dist/app/components/welcome.vue")['default']>
     'LazyNuxtLayout': LazyComponent<typeof import("../node_modules/nuxt/dist/app/components/nuxt-layout")['default']>
     'LazyNuxtErrorBoundary': LazyComponent<typeof import("../node_modules/nuxt/dist/app/components/nuxt-error-boundary")['default']>
@@ -66,6 +68,7 @@ declare module 'vue' {
   export interface GlobalComponents extends _GlobalComponents { }
 }
 
+export const Button: typeof import("../src/components/Button.vue")['default']
 export const TheHeader: typeof import("../src/components/TheHeader.vue")['default']
 export const NuxtWelcome: typeof import("../node_modules/nuxt/dist/app/components/welcome.vue")['default']
 export const NuxtLayout: typeof import("../node_modules/nuxt/dist/app/components/nuxt-layout")['default']
@@ -90,6 +93,7 @@ export const Html: typeof import("../node_modules/nuxt/dist/head/runtime/compone
 export const Body: typeof import("../node_modules/nuxt/dist/head/runtime/components")['Body']
 export const NuxtIsland: typeof import("../node_modules/nuxt/dist/app/components/nuxt-island")['default']
 export const NuxtRouteAnnouncer: IslandComponent<typeof import("../node_modules/nuxt/dist/app/components/server-placeholder")['default']>
+export const LazyButton: LazyComponent<typeof import("../src/components/Button.vue")['default']>
 export const LazyTheHeader: LazyComponent<typeof import("../src/components/TheHeader.vue")['default']>
 export const LazyNuxtWelcome: LazyComponent<typeof import("../node_modules/nuxt/dist/app/components/welcome.vue")['default']>
 export const LazyNuxtLayout: LazyComponent<typeof import("../node_modules/nuxt/dist/app/components/nuxt-layout")['default']>
---
+++ my-nuxt-app/src/components/Button.vue
@@ -0,0 +1,44 @@
+<template>
+  <button
+    :class="[
+      'rounded-sm transition-all duration-200 border',
+      sizeClasses[size],
+      variantClasses[variant],
+      { 'border-2': hasBorder }
+    ]"
+    v-bind="$attrs"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+type ButtonSize = 'sm' | 'md' | 'lg' | 'xl'
+type ButtonVariant = 'white' | 'black' | 'transparentDark' | 'transparentLight'
+
+interface Props {
+  size?: ButtonSize
+  variant?: ButtonVariant
+  hasBorder?: boolean
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  size: 'md',
+  variant: 'white',
+  hasBorder: false
+})
+
+const sizeClasses = {
+  sm: 'px-3 py-1 text-sm',
+  md: 'px-4 py-2',
+  lg: 'px-6 py-3 text-lg',
+  xl: 'px-8 py-4 text-xl'
+}
+
+const variantClasses = {
+  white: 'bg-white text-black border-gray-200 hover:shadow-md',
+  black: 'bg-black text-white border-gray-800 hover:shadow-md',
+  transparentDark: 'bg-transparent text-gray-800 border-gray-800 hover:shadow-md',
+  transparentLight: 'bg-transparent text-white border-white hover:shadow-md'
+}
+</script>
